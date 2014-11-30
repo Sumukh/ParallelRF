a6c5e83ed87552561b338d37203e87541e71cc2a
Michael Huston <michaelhuston@Michaels-MacBook-Pro-4.local>
2014-11-29 19:57:10 -0800

    Add python script to generate mnist datasets for application.

---
+++ mnist.py
@@ -0,0 +1,25 @@
+# Script used to create Mnist_mini and Mnist_full datasets.
+
+import numpy as np
+from sklearn.datasets import fetch_mldata
+from pandas import DataFrame
+
+# Default download location for caching is 
+# ~/scikit_learn_data/mldata/mnist-original.mat unless specified otherwise.
+mnist = fetch_mldata('MNIST original')
+
+# Create DataFrame, group data by class.
+df = DataFrame(mnist.data)
+df['class'] = mnist.target
+grouped = df.groupby('class')
+
+# Write data feature values to file in Dataset directory by class.
+for name, group in grouped:
+	# Create mini binary MNIST classification dataset for faster testing.
+	if int(name) < 2:
+		fname = 'Dataset/Mnist_mini/Class' + str(int(name)) + '.txt'
+		np.savetxt(fname, group[:200])
+
+	# Create full MNIST classification for full application.
+	fname = 'Dataset/Mnist_full/Class' + str(int(name)) + '.txt'
+	np.savetxt(fname, group)
