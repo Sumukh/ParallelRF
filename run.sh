305d621d879c1445d3188e37bb200af95b822772
Ubuntu <ubuntu@ip-10-46-130-49.us-west-2.compute.internal>
2014-12-10 13:12:21 +0000

    change run script

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
   
 #!bin/bash
-"trees;threads;time;error" > output.csv
-for t in {1..16}
+echo "trees;threads;time;error" > output.csv
+for t in 32 48 50 58 64 68 72 78 82 86
         do
                 for i in 1 2 4 8 16 32 64 128
                 do
-                        ./prog -n $i -t $t -v FiveFold -p 1 >> output.csv
+                        echo $(./prog -n $i -t $t -v FiveFold  -p 1) >> output.csv
                 done
         done
